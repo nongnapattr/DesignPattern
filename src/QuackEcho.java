490e387fe34e6ab17727c3a9f7de076c7daab60c
nongnapat <nongnapat.tr@ku.th>
2022-08-17 18:14:50 +0700

    first commit

---
+++ src/QuackEcho.java
@@ -0,0 +1,14 @@
+public class QuackEcho implements Quackable{
+    private Quackable duck;
+
+    public QuackEcho(Quackable duck) {
+        this.duck = duck;
+    }
+
+    @Override
+    public void quack() {
+        duck.quack();
+        System.out.println("Echo:");
+        duck.quack();
+    }
+}
